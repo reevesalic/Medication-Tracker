46f70e350fe685329b1eef456ae7acdd6c0c232a
reevesalic <reevesalic@gmail.com>
2020-11-23 12:51:07 -0500

    scope changes

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,8 +4,11 @@ class Patient < ApplicationRecord
      has_many :medications, :through => :illnesses
      accepts_nested_attributes_for :illnesses 
      
+
+     # scope :search(params), -> { where("LOWER(name) LIKE ?", ("params")) }
+     # scope :search, ->(name_param) {where(:name == name_param)}
+
      def self.search(params)
           where("LOWER(name) LIKE ?", "%#{params}%")
-     # binding.pry
      end
 end
