914c2352698b543119d3bd7232abe233f8e2dca1
reevesalic <reevesalic@gmail.com>
2020-12-27 16:43:17 -0500

    removed links for login and signup pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,22 +4,24 @@ skip_before_action :require_login
     def welcome
     end
 
-    def  destroy
+    def  destroy #deletes the user object
       session.delete(:user_id)
       redirect_to '/'
     end
 
-    def new
+    def new #login form
     end
 
-    def create
+    def create #log in
+      #find user object
       @user = User.find_by(email: params[:user][:email])
+      #if user exists, then authenticate
       if @user.try(:authenticate, params[:user][:password])
         session[:user_id] = @user.id
         redirect_to user_path(@user) 
       else
         flash[:error] = "Sorry, please try again."
-        render :welcome
+        redirect_to login_path
       end
     end
 
