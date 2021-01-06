14320ecab2991b9cb6b2ae7af7fcfe44bf018aef
reevesalic <reevesalic@gmail.com>
2021-01-06 16:00:04 -0500

    added view and controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,10 @@
 class CategoriesController < ApplicationController
+     def show
+          @category = Category.find(params[:id])
+          @illness = Illness.find(params[:id])
+     end
+
+     def show
+          @category = Category.all
+     end
 end
